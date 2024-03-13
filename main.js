0a2ab5240bd9457266805a0f9812d645b44dc469
PalomequeMaria30 <163175425+PalomequeMaria30@users.noreply.github.com>
2024-03-12 20:53:54 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function muestra_oculta (contenido){
+    let div = document.getElementById (contenido);
+    if (div.style.display == "none"){
+        div.style.display = "flex";
+    }
+    else{
+        div.style.display = "none";
+    }
+}
+function muestra_oculta (contenido2){
+    let div = document.getElementById (contenido2);
+    if (div.style.display == "none"){
+        div.style.display = "flex";
+    }
+    else{
+        div.style.display = "none";
+    }
+}
+function muestra_oculta (contenido3){
+    let div = document.getElementById (contenido3);
+    if (div.style.display == "none"){
+        div.style.display = "flex";
+    }
+    else{
+        div.style.display = "none";
+    }
+}
+function muestra_oculta (contenido4){
+    let div = document.getElementById (contenido4);
+    if (div.style.display == "none"){
+        div.style.display = "flex";
+    }
+    else{
+        div.style.display = "none";
+    }
+}
+function muestra_oculta (contenido5){
+    let div = document.getElementById (contenido5);
+    if (div.style.display == "none"){
+        div.style.display = "flex";
+    }
+    else{
+        div.style.display = "none";
+    }
+}
